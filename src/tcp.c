ca7271b66b105df0362051509fa9bfcd7726e303
Janae <janae.eimer@gmail.com>
2026-07-13 22:25:18 -0400

    edge added for OOB port

--- src/tcp.c
+++ src/tcp.c
@@ -15,7 +15,7 @@ server_status_e bind_tcp_port(tcp_server *server, int port){
     //edge case for port OOB
     if(port < 0 || port > 65535){
         printf("Invalid port number\n");
-        return SERVER_BIND_ERROR;
+        return SERVER_SOCKET_ERROR;
     }
 
     server->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
