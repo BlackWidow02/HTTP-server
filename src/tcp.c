e725f90e9297a8fc5370bfd25af62ed0a98c48c9
Janae <janae.eimer@gmail.com>
2026-07-16 17:58:51 -0400

    edge added for OOB port

--- src/tcp.c
+++ src/tcp.c
@@ -16,7 +16,7 @@ server_status_e bind_tcp_port(tcp_server *server, int port){
     server->socket_fd = -1;
     
     //edge case for port OOB
-    if(port < 0 || port > 65535){
+    if(port && port > 65535)){
         return SERVER_BIND_ERROR;
     }
     
