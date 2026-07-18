cddd52acca01d5da6e4d7100467506873673a186
Janae <janae.eimer@gmail.com>
2026-07-17 21:47:03 -0400

    edge case added for OOB port

--- src/tcp.c
+++ src/tcp.c
@@ -12,13 +12,13 @@ server_status_e bind_tcp_port(tcp_server *server, int port){
     //type is SOCK_STREAM
     //protocal is 0
 
+    memset(server, 0, sizeof(*server)); //zero out all fields first
+    server->socket_fd = -1;
+
     //edge case for port OOB
     if(port < 1 || port > 65535){
         return -1;
     }
-
-     memset(server, 0, sizeof(*server)); //zero out all fields first
-    server->socket_fd = -1;
     
     server->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(server->socket_fd == -1){
