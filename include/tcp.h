42f4bcb203905a485032b3e6518479fb3cd0cd28
Janae <janae.eimer@gmail.com>
2026-07-17 21:23:33 -0400

    edge case added for OOB port

--- include/tcp.h
+++ include/tcp.h
@@ -1,9 +1,10 @@
 #ifndef TCP_H
 #define TCP_H
 
-#include <arpa/inet.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MAX_CONNECTIONS 8
 
 typedef enum {
     SERVER_OK,
@@ -18,7 +19,8 @@ typedef struct {
     int socket_fd;
 } tcp_server;
 
+
 server_status_e bind_tcp_port(tcp_server *server, int port);
 int accept_client(int server_fd);
 
-#endif // TCP_H
+#endif
